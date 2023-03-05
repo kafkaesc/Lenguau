e938df50125a15a02a91dd6911542e9e78d6c92d
kafkaesc <jared.a.hettinger@gmail.com>
2023-03-04 20:54:50 -0600

    Creating the vocabBox hook. Implementing on the two column match exercise.

---
+++ src/hooks/useVocabBox.js
@@ -0,0 +1,104 @@
+const colorsVocab = [
+	{ en: 'black', es: 'negro/a' },
+	{ en: 'blue', es: 'azul' },
+	{ en: 'gold', es: 'dorado/a' },
+	{ en: 'gray', es: 'gris' },
+	{ en: 'green', es: 'verde' },
+	{ en: 'orange', es: 'anaranjado/a' },
+	{ en: 'pink', es: 'rosado/a' },
+	{ en: 'purple', es: 'morado/a' },
+	{ en: 'yellow', es: 'amarillo/a' },
+	{ en: 'red', es: 'rojo/a' },
+	{ en: 'silver', es: 'plateaado/a' },
+	{ en: 'white', es: 'blanco/a' },
+];
+
+const literatureVocab = [
+	{ en: 'book', es: 'el libro' },
+	{ en: 'chapter', es: 'el capítulo' },
+	{ en: 'character', es: 'el personaje' },
+	{ en: 'comedy', es: 'la comedia' },
+	{ en: 'drama', es: 'el drama' },
+	{ en: 'fantasy', es: 'la literatura fantastica' },
+	{ en: 'genre', es: 'el género' },
+	{ en: 'horror', es: 'el terror' },
+	{ en: 'literature', es: 'la literatura' },
+	{ en: 'novel', es: 'la novela' },
+	{ en: 'novella', es: 'la novela corta' },
+	{ en: 'satire', es: 'la sátira' },
+	{ en: 'science fiction', es: 'la ciencia ficción' },
+	{ en: 'setting', es: 'el ambientación' },
+	{ en: 'short story', es: 'el cuento' },
+	{ en: 'theme', es: 'el tema' },
+	{ en: 'tragedy', es: 'la tragedia' },
+];
+
+const officeVocab = [
+	{ en: 'boss', es: 'el jefe/la jefa' },
+	{ en: 'calculator', es: 'la calculadora' },
+	{ en: 'chair', es: 'la silla' },
+	{ en: 'computer', es: 'la computadora' },
+	{ en: 'coworker', es: 'el colega/la colega' },
+	{ en: 'desk', es: 'el escritorio' },
+];
+
+const townVocab = [
+	{ en: 'airport', es: 'el aeropuerto' },
+	{ en: 'bakery', es: 'la panadería' },
+	{ en: 'bank', es: 'el banco' },
+	{ en: 'bar', es: 'el bar' },
+	{ en: 'bookstore', es: 'la librería' },
+	{ en: 'bus stop', es: 'la parada de autobús' },
+];
+
+let __vocabBox = [];
+let __roundSize = 0;
+
+function getRound(round) {
+	console.log('round: ', round);
+	if (__vocabBox.length === 0) {
+		console.error(
+			'pop was called on the __vocabBox, but there is no data to return.'
+		);
+	}
+	if (__vocabBox.length > 0) {
+		const start = (round - 1) * __roundSize;
+		const end = start + __roundSize;
+
+		return __vocabBox.slice(start, end);
+	}
+}
+
+function hasRound(round) {
+	return (round - 1) * __roundSize >= __vocabBox.length ? false : true;
+}
+
+// TODO: Update this to call the API.
+export function useVocabBox(categoryTitle, roundSize) {
+	let loaded = false;
+	__roundSize = roundSize;
+	switch (categoryTitle) {
+		case 'Around-Town':
+			__vocabBox = [...townVocab];
+			loaded = true;
+			break;
+		case 'At-the-Office':
+			__vocabBox = [...officeVocab];
+			loaded = true;
+			break;
+		case 'Colors':
+			__vocabBox = [...colorsVocab];
+			loaded = true;
+			break;
+		case 'Literature':
+			__vocabBox = [...literatureVocab];
+			loaded = true;
+			break;
+		default:
+			console.error(
+				'No vocabulary to match the provided category title: ' + categoryTitle
+			);
+			break;
+	}
+	return { getRound, hasRound, loaded };
+}
--- src/pages/TwoColumnMatch.jsx
+++ src/pages/TwoColumnMatch.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { shuffle } from 'utilities/GameUtil';
+import { useVocabBox } from 'hooks/useVocabBox';
 
 import LenguaSpan from 'elements/LenguaSpan';
 import P from 'elements/P';
@@ -10,56 +11,20 @@ import PageTitle from 'layout/PageTitle';
 
 import TcMatchSingleColumn from 'components/TcMatchSingleColumn';
 
-const colorsVocab = [
-	{ en: 'black', es: 'negro/a' },
-	{ en: 'blue', es: 'azul' },
-	{ en: 'gold', es: 'dorado/a' },
-	{ en: 'gray', es: 'gris' },
-	{ en: 'green', es: 'verde' },
-	{ en: 'orange', es: 'anaranjado/a' },
-	{ en: 'pink', es: 'rosado/a' },
-	{ en: 'purple', es: 'morado/a' },
-	{ en: 'yellow', es: 'amarillo/a' },
-	{ en: 'red', es: 'rojo/a' },
-	{ en: 'silver', es: 'plateaado/a' },
-	{ en: 'white', es: 'blanco/a' },
-];
-
-const literatureVocab = [
-	{ en: 'book', es: 'el libro' },
-	{ en: 'comedy', es: 'la comedia' },
-	{ en: 'drama', es: 'el drama' },
-	{ en: 'fantasy', es: 'la literatura fantastica' },
-	{ en: 'genre', es: 'el género' },
-];
-
-const officeVocab = [
-	{ en: 'boss', es: 'el jefe/la jefa' },
-	{ en: 'calculator', es: 'la calculadora' },
-	{ en: 'chair', es: 'la silla' },
-	{ en: 'computer', es: 'la computadora' },
-	{ en: 'coworker', es: 'el colega/la colega' },
-	{ en: 'desk', es: 'el escritorio' },
-];
-
-const townVocab = [
-	{ en: 'airport', es: 'el aeropuerto' },
-	{ en: 'bakery', es: 'la panadería' },
-	{ en: 'bank', es: 'el banco' },
-	{ en: 'bar', es: 'el bar' },
-	{ en: 'bookstore', es: 'la librería' },
-	{ en: 'bus stop', es: 'la parada de autobús' },
-];
+const ROUND_SIZE = 6;
 
 export default function TwoColumnMatch() {
 	const [clearedCount, setClearedCount] = useState(0);
 	const [columns, setColumns] = useState({ l: [], r: [] });
 	const [correctCount, setCorrectCount] = useState(0);
+	const [finished, setFinished] = useState(false);
 	const [lValue, setLValue] = useState({});
+	const [round, setRound] = useState(1);
 	const [rValue, setRValue] = useState({});
 	const [wrongCount, setWrongCount] = useState(0);
 
 	const { categoryTitle } = useParams();
+	const vocabBox = useVocabBox(categoryTitle, ROUND_SIZE);
 
 	function clearSelected() {
 		setLValue({});
@@ -108,47 +73,40 @@ export default function TwoColumnMatch() {
 
 	useEffect(() => {
 		if (clearedCount === columns.l.length) {
-			const newColumns = {};
-			newColumns.l = columns.l.map((vo) => {
-				return { ...vo, cleared: false };
-			});
-			newColumns.r = columns.r.map((vo) => {
-				return { ...vo, cleared: false };
-			});
-			setClearedCount(0);
-			setColumns(newColumns);
+			if (vocabBox.hasRound(round + 1)) {
+				let newColumns = [];
+				const roundVocab = vocabBox.getRound(round + 1);
+				newColumns.l = shuffle(
+					roundVocab.map((vo) => {
+						return { ...vo, cleared: false };
+					})
+				);
+				newColumns.r = shuffle(
+					roundVocab.map((vo) => {
+						return { ...vo, cleared: false };
+					})
+				);
+				setClearedCount(0);
+				setColumns(newColumns);
+				setRound((prev) => prev + 1);
+			} else {
+				setFinished(true);
+			}
 		}
-		// TODO: Make this load n rows at a time from a vocabulary list.
-		// The use of columns.l.length will be replaced with a const value.
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [clearedCount]);
 
 	useEffect(() => {
-		let vocabList = [];
-		switch (categoryTitle) {
-			case 'Around-Town':
-				vocabList = [...townVocab];
-				break;
-			case 'At-the-Office':
-				vocabList = [...officeVocab];
-				break;
-			case 'Colors':
-				vocabList = [...colorsVocab];
-				break;
-			case 'Literature':
-				vocabList = [...literatureVocab];
-				break;
-			default:
-				console.error(
-					'No vocabulary to match the provided category title: ' + categoryTitle
-				);
-				break;
-		}
+		const vocabList = vocabBox.getRound(1).map((vo) => {
+			return { ...vo, cleared: false };
+		});
 		const newCols = {
 			l: shuffle(vocabList),
 			r: shuffle(vocabList),
 		};
 		setColumns(newCols);
+		setRound(1);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [categoryTitle]);
 
 	return (
@@ -158,25 +116,31 @@ export default function TwoColumnMatch() {
 					<LenguaSpan en="Duo Match" es="Combinar los Dúos" />
 				</PageTitle>
 				<P className="text-center">
-					Correct: {correctCount}; Wrong: {wrongCount};
+					Round: {round}; Correct: {correctCount}; Wrong: {wrongCount};
 				</P>
 			</div>
-			<div className="inline-block w-1/2 p-1">
-				<TcMatchSingleColumn
-					languageCode="en"
-					select={selectLeftColumn}
-					selectedVocab={lValue}
-					vocab={columns.l}
-				/>
-			</div>
-			<div className="inline-block w-1/2 p-1">
-				<TcMatchSingleColumn
-					languageCode="es"
-					select={selectRightColumn}
-					selectedVocab={rValue}
-					vocab={columns.r}
-				/>
-			</div>
+			{finished ? (
+				<P className="text-center">Victory!!</P>
+			) : (
+				<>
+					<div className="inline-block w-1/2 p-1">
+						<TcMatchSingleColumn
+							languageCode="en"
+							select={selectLeftColumn}
+							selectedVocab={lValue}
+							vocab={columns.l}
+						/>
+					</div>
+					<div className="inline-block w-1/2 p-1">
+						<TcMatchSingleColumn
+							languageCode="es"
+							select={selectRightColumn}
+							selectedVocab={rValue}
+							vocab={columns.r}
+						/>
+					</div>
+				</>
+			)}
 		</AppBody>
 	);
 }
