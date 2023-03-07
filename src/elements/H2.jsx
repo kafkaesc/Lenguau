18a348eb434bed74727c6bae77b61ddc215441a6
kafkaesc <jared.a.hettinger@gmail.com>
2023-03-07 10:46:00 -0600

    Creating H2 component. Updating H1 and P.

--- src/elements/H2.jsx
+++ src/elements/H2.jsx
@@ -2,5 +2,5 @@ export default function H1(props) {
 	// This is a wrapper component--the element content
 	// should be passed via props.
 	// eslint-disable-next-line
-	return <h1 {...props} className="mb-2 text-4xl text-center"></h1>;
+	return <h1 {...props} className="mb-2 text-2xl text-center"></h1>;
 }
--- src/elements/P.jsx
+++ src/elements/P.jsx
@@ -2,6 +2,6 @@ export default function P(props) {
 	return props.className ? (
 		<p {...props} className={`mb-2 ${props.className}`}></p>
 	) : (
-		<p {...props} className="mb-2 "></p>
+		<p {...props} className="mb-2"></p>
 	);
 }
