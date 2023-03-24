2ea5162846aff663778217e534a7a0cbf677f6ce
kafkaesc <jared.a.hettinger@gmail.com>
2023-03-24 11:04:07 -0500

    Loading category menu from an object

--- src/layout/CategoryMenu.jsx
+++ src/layout/CategoryMenu.jsx
@@ -4,24 +4,19 @@ import LenguaSpan from 'elements/LenguaSpan';
 import MenuButton from 'elements/MenuButton';
 
 export default function CategoryMenu() {
+	const {
+		categoryList: categories,
+	} = require(`assets/offline-data/set-categories.json`);
 	const gamePath = useLocation().pathname.split('/')[1];
 	return (
 		<ul>
-			<MenuButton to={`/${gamePath}/Around-Town`}>
-				<LenguaSpan en="Around Town" es="En el Pueblo" />
-			</MenuButton>
-			<MenuButton to={`/${gamePath}/At-the-Office`}>
-				<LenguaSpan en="At the Office" es="En la Oficina" />
-			</MenuButton>
-			<MenuButton to={`/${gamePath}/Colors`}>
-				<LenguaSpan en="Colors" es="De Colores" />
-			</MenuButton>
-			<MenuButton to={`/${gamePath}/Literature`}>
-				<LenguaSpan en="Literature" es="La Literatura" />
-			</MenuButton>
-			<MenuButton to={`/${gamePath}/Top-50-Verbs`}>
-				<LenguaSpan en="Top 50 Verbs" es="50 Verbos Principales" />
-			</MenuButton>
+			{categories.map((ca) => {
+				return (
+					<MenuButton to={`/${gamePath}/${ca.kebab}`}>
+						<LenguaSpan en={ca.en} es={ca.es} />
+					</MenuButton>
+				);
+			})}
 		</ul>
 	);
 }
