1d7414b92c772e98cb88dd484ee2b7d313a29073
kafkaesc <jared.a.hettinger@gmail.com>
2023-03-28 15:02:36 -0500

    Updating fetch call for CategoryMenu. Updating gitignore.

--- src/layout/CategoryMenu.jsx
+++ src/layout/CategoryMenu.jsx
@@ -1,18 +1,41 @@
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import { useLenguaApi } from 'context/LenguaApiContext';
 
 import LenguaSpan from 'elements/LenguaSpan';
 import MenuButton from 'elements/MenuButton';
 
 export default function CategoryMenu() {
-	const {
+	const [categories, setCategories] = useState([]);
+
+	/*const {
 		categoryList: categories,
-	} = require(`assets/offline-data/set-categories.json`);
+	} = require(`assets/offline-data/set-categories.json`);*/
 	const gamePath = useLocation().pathname.split('/')[1];
+	const apiBase = useLenguaApi();
+
+	useEffect(() => {
+		async function myFetch() {
+			fetch(`${apiBase}vocab/`)
+				.then((res) => res.json())
+				.then((data) => {
+					setCategories(data.categoryList);
+				})
+				.catch((err) => {
+					console.error(
+						'Error fetching the category data for the category menu'
+					);
+				});
+		}
+
+		myFetch();
+	}, [apiBase]);
+
 	return (
 		<ul>
-			{categories.map((ca) => {
+			{categories.map((ca, i) => {
 				return (
-					<MenuButton to={`/${gamePath}/${ca.kebab}`}>
+					<MenuButton key={i} to={`/${gamePath}/${ca.kebab}`}>
 						<LenguaSpan en={ca.en} es={ca.es} />
 					</MenuButton>
 				);
