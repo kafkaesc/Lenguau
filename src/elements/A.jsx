0592ca3ab8d114cbcd14070196994b17021cd590
kafkaesc <jared.a.hettinger@gmail.com>
2023-04-27 12:54:05 -0500

    Adding children prop for A, H1, H2 components.

--- src/elements/A.jsx
+++ src/elements/A.jsx
@@ -1,9 +1,11 @@
-// This is a wrapper component--the element content should be passed via props.
-/* eslint-disable jsx-a11y/anchor-has-content */
-export default function A({ className, ...props }) {
+export default function A({ children, className, ...props }) {
 	return className ? (
-		<a {...props} className={`text-blue hover:underline ${className}`}></a>
+		<a {...props} className={`text-blue hover:underline ${className}`}>
+			{children}
+		</a>
 	) : (
-		<a {...props} className="text-blue hover:underline"></a>
+		<a {...props} className="text-blue hover:underline">
+			{children}
+		</a>
 	);
 }
--- src/elements/H2.jsx
+++ src/elements/H2.jsx
@@ -1,9 +1,11 @@
-/* This is a wrapper component--the element content should be passed via props. */
-/* eslint-disable jsx-a11y/heading-has-content */
-export default function H2({ className, ...props }) {
+export default function H2({ children, className, ...props }) {
 	return className ? (
-		<h2 {...props} className={`mb-2 text-2xl text-center ${className}`}></h2>
+		<h2 {...props} className={`mb-2 text-2xl text-center ${className}`}>
+			{children}
+		</h2>
 	) : (
-		<h2 {...props} className="mb-2 text-2xl text-center"></h2>
+		<h2 {...props} className="mb-2 text-2xl text-center">
+			{children}
+		</h2>
 	);
 }
