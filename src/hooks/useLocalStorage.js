7a802e9680e259588369c631aa398ed8c9ae856b
kafkaesc <jared.a.hettinger@gmail.com>
2023-12-06 16:06:33 -0600

    Adding useLocalStorage hook

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,12 @@
+// TODO: Built out these functions
+export function useLocalStorage() {
+	function add(key, val) {}
+
+	function clearAll(key) {}
+
+	function clear(key) {}
+
+	function get(key) {}
+
+	return { add, clearAll, clear, get };
+}
