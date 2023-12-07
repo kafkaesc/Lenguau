41657cc2c4661fc45f2a6b51cb9905bdfb8cfca6
kafkaesc <jared.a.hettinger@gmail.com>
2023-12-07 10:16:47 -0600

    Storing the users language preference in localstorage

--- src/layout/LenguaToggle.jsx
+++ src/layout/LenguaToggle.jsx
@@ -14,9 +14,9 @@ export default function LenguaToggle() {
 			className="w-48 text-sm border-2 border-black"
 		>
 			{lengua === 'en' ? (
-				<span className="inline-block w-1/2 text-white bg-black">English</span>
+				<span className="inline-block w-1/2 text-white bg-black">English </span>
 			) : (
-				<span className="inline-block w-1/2">English</span>
+				<span className="inline-block w-1/2">English </span>
 			)}
 			{lengua === 'es' ? (
 				<span className="inline-block w-1/2 text-white bg-black">Español</span>
